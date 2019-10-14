abccad3c65c3e01313b427f13c628415890245b2
tawfiqsiam <toto.siam999@gmail.com>
2019-10-14 23:35:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -183,6 +183,35 @@ client.on('message', message => {
                })
                }
                });
- 
+ const Discord = require('discord.js');
+
+module.exports = (client, guild) => {
+    try {
+        client.user.setActivity(`$help | $bc`, { type: 'PLAYING' });
+
+        let bots = 0;
+        let people = 0;
+        guild.members.forEach(mem => {
+            if (mem.user.bot) { bots += 1; }
+            else {
+                people += 1;
+            }
+        });
+
+        let logChannel = client.config.logChannel;
+
+        let embed = new Discord.RichEmbed()
+            .setColor(client.config.embedColor)
+            .setTitle(`Just __joined__ ${guild.name}`)
+            .setDescription(`**${guild.owner.user.username}#${guild.owner.user.discriminator}** is the owner of the guild.\nGuild has **${guild.members.size}** members.\n\n`)
+            .addField("People: ", people, true)
+            .addField("Bots: ", bots, true)
+            .setTimestamp();
+
+        client.channels.get(logChannel).send(embed);
+    } catch (err) {
+        console.log(err);
+    }
+}
  
 client.login(process.env.BOT_TOKEN);
