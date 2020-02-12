a9d1e0b211bca497d6818113ac4a0d7d9af519cd
tawfiqsiam <toto.siam999@gmail.com>
2020-02-12 13:14:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ client.on('message', message => {
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}\n By Server : ${message.guild.name}`);
+ m.send(`${argresult}\n ${m} `);
 })
  message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : Reserved `); 
  message.delete(); 
