8fe9ec237fede389d0d4531587843115a6a69f3f
tawfiqsiam <toto.siam999@gmail.com>
2020-03-13 05:57:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -198,6 +198,17 @@ if (message.content.startsWith(prefix + 'setava')) {
 });
 
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-abc')){
+if (message.author.id !== '263639609416613888') return message.reply('** You need to be a donator to do this coand   **')
+message.channel.sendMessage('Cool Stuff are happening    |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
