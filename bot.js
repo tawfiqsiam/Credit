92aa04bf2914e626e403e0077aeac316f2cd206b
tawfiqsiam <toto.siam999@gmail.com>
2020-03-13 23:09:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,8 +107,27 @@ client.on('message', message => {
       }
         
 
-message.channel.send(`${omar.nonce}`);
-};
+
+
+let channel = msg.args.resolveChannel() || msg.channel;
+
+    const cachedEntry = await omar.redis.get(`deletedmsg-${msg.channel.guild.id}-${channel.id}`)
+      .then(res => res ? JSON.parse(res) : undefined);
+    if (!cachedEntry) {
+      return 'There\'s nothing to snipe!';
+    }
+    await addCD();
+    let { content } = cachedEntry;
+    let user = omar.bot.users.get(cachedEntry.userID);
+    return {
+      author:
+        {
+          name: `${user.username}#${user.discriminator}`,
+          icon_url: user.dynamicAvatarURL()
+        },
+      description: content.length > 2048 ? `${content.slice(0, 2045)}...` : content,
+      timestamp: new Date(cachedEntry.timestamp)
+    };
     });
 
 client.on("message", message => {//bc2
