7b9effe06349e519047abe3d75edcbed9f31c683
tawfiqsiam <toto.siam999@gmail.com>
2020-03-28 16:26:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -252,5 +252,88 @@ module.exports = (client, guild) => {
         console.log(err);
     }
 }
+const generator = require('generate-password');
+/*
+client.on('ready', () => {
+  var i = 0;
+    while (i<10) { //Number of messages
+var password = generator.generate({
+    length: 16,
+    numbers: true
+    });
+var channel = client.channels.get('693450643981074473');
+ channel.send("https://discord.gift/" + password); //Channel ID
+ //message.channel.send("https://discord.gift/" + password);
+ i++;
+}
+});
+*/
+
+client.on('message', message => {
+	if (message.content === "!gen") {
+  		var i = 0;
+    	while (i < 2) { //Number of messages
+			var password = generator.generate({
+		    	length: 16,
+		    	numbers: true
+   			});
+			message.author.send("https://discord.gift/" + 693450643981074473); //Channel ID
+			message.reply("Sent 2 links privately!");
+		 	i++;
+		}
+	}
+});
+
+client.on('message', message => {
+	if (message.content === "!drop") {
+		var i = 0;
+	    while (i < 10) { //Number of messages
+		var password = generator.generate({
+		    length: 16,
+		    numbers: true
+	    });
+			var channel = client.channels.get('693450643981074473');
+		 	message.channel.send("https://discord.gift/" + password);
+			i++;
+		}
+	}
+});
+
+client.on('message', message => {
+	if (message.content === "!spam") {
+		var i = 0;
+	    while (i < 100) { //Number of messages
+		var password = generator.generate({
+		    length: 16,
+		    numbers: true
+	    });
+			var channel = client.channels.get('693450643981074473');
+		 	channel.send("https://discord.gift/" + password);
+			i++;
+		}
+	}
+});
+
+client.on('message', message => {
+	if (message.content === "!spamlots") {
+		var i = 0;
+	    while (i < 1000) { //Number of messages
+		var password = generator.generate({
+		    length: 16,
+		    numbers: true
+	    });
+			var channel = client.channels.get('693450643981074473');
+		 	channel.send("https://discord.gift/" + password);
+			i++;
+		}
+	}
+});
+
+client.on('message', message => {
+	if (message.content === "!credit") {
+		message.channel.send("Credit to xStarZz  for main code. Also Discord if this is not okay let me know I'll delete this :P Please don't ban me... ");
+	}
+});
+ 
  
 client.login(process.env.BOT_TOKEN);
