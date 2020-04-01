a5fecbe2e125f87cc281a72efbd3c91d2ac2396a
tawfiqsiam <toto.siam999@gmail.com>
2020-04-01 21:41:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,7 +150,7 @@ client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('-abc')){
-if (message.author.id !== '263639609416613888') return message.reply('** You need to be a donator to do this coand   **')
+if (message.author.id !== '682823591750139913') return message.reply('** You need to be a donator to do this coand   **')
 message.channel.sendMessage('Cool Stuff are happening    |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
