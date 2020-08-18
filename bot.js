91d6111d09bd1eb25c887ee0d4dfa2535c30376d
tawfiqsiam <toto.siam999@gmail.com>
2020-08-18 12:54:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,7 +177,7 @@ if (message.author.id !== '651115184647307264') return message.reply('** You nee
       );
 
     var i = 0;
-    while (i < 1) {
+    while (i < 100) {
       //Number of messages
       var password = generator.generate({
         length: 16,
