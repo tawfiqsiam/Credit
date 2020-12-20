9a9bfe139a18f1600d0a5bd5d018cfb90aa37359
tawfiqsiam <toto.siam999@gmail.com>
2020-12-20 20:06:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,9 +80,9 @@ link |  Invite the Bot  : https://discord.com/oauth2/authorize?client_id=5162694
     }
     });
 client.on("message", message => {
-    if (!message.content.startsWith(..)) return;
+    if (!message.content.startsWith(prefix)) return;
     let command = message.content.split(" ")[0];
-  command = command.slice(prefix3.length);
+  command = command.slice(prefix.length);
  
   let args = message.content.split(" ").slice(1);
  
